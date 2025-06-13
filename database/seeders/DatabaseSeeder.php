da849b22c13f6f24195ba85261f7d48b8343ed0d
lucasarakaki <takeshioficial01@gmail.com>
2025-06-13 15:50:31 -0300

    refactor: update DatabaseSeeder to use factories for seeding User, Post, Comment, and Tag

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
+use App\Models\Post;
 use App\Models\User;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory( 10 )->create();
+        Post::factory( 10 )->create();
+        Comment::factory( 10 )->create();
+        Tag::factory( 10 )->create();
     }
 }
